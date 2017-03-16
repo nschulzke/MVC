a3de99b2e99b01e0746ebc9495ca973f6864fc8b
Nathan Schulzke <nschulzke@gmail.com>
2017-03-16 11:30:18 -0600

    Create layout.php, implement index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 require_once('classes/Connection.php');
+
+if (isset($_GET['controller']) && isset($_GET['action'])) {
+    $controller = $_GET['controller'];
+    $action = $_GET['action'];
+} else {
+    $controller = 'pages';
+    $action = 'home';
+}
+
+require_once __DIR__ . '/../config/DBConfig.php';
+
 ?>
---
+++ views/layout.php
@@ -0,0 +1,14 @@
+<DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <header>
+        </header>
+        
+        <?php require_once __DIR__ . '/../config/DBConfig.php'; ?>
+        
+        <footer>
+        </footer>
+    </body>
+</html>
