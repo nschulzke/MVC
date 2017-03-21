f7c0a46c7af57f2303350cfd83b65638471fe736
Nathan Schulzke <nschulzke@gmail.com>
2017-03-21 00:05:26 -0600

    Add option to set title in html data

--- rsc/js/base.js
+++ rsc/js/base.js
@@ -1,11 +1,14 @@
 $('#dynamic-modal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) // Button that triggered the modal
-    var url = button.data('url')        // Extract info from data-* attributes
+    var button = $(event.relatedTarget);    // Button that triggered the modal
+    var url = button.data('url');           // Extract info from data-* attributes
+    var title = button.data('title');
+    if (title == undefined)
+        title = '';
     
-    var modal = $(this)
+    var modal = $(this);
     
+    modal.find('#dynamic-modal-title').html(title);
     $.get(url, { layout: "none" }, function(data) {
-        console.log("success!");
         modal.find('#dynamic-modal-body').html(data);
     });
 });
--- view/static-pages/test.php
+++ view/static-pages/test.php
@@ -1,1 +1,2 @@
 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#dynamic-modal" data-url="<?= GlobalConfig::getAppPath() ?>/about">Open About</button>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#dynamic-modal" data-url="<?= GlobalConfig::getAppPath() ?>/home" data-title="Home">Open Home</button>
