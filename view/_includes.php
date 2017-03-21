ce04eeed4ed49b0b236b9707094d79d028d48d3d
Nathan Schulzke <nschulzke@gmail.com>
2017-03-20 20:37:38 -0600

    Fix script source, reorder

--- view/_includes.php
+++ view/_includes.php
@@ -1,6 +1,6 @@
-<link rel="stylesheet" type="text/css" href="<?= GlobalConfig::getAppPath() ?>/rsc/lib/bootstrap-3.3.7/css/bootstrap.css">
-<script src="<?= GlobalConfig::getAppPath() ?>/rsc/lib/bootstrap-3.3.7/css/bootstrap.js"></script>
-<script src="<?= GlobalConfig::getAppPath() ?>/rsc/lib/jquery-3.2.0/jquery.js"></script>
+<script src="<?= GlobalConfig::getAppPath() ?>/rsc/lib/jquery-3.2.0/jquery.js">
+</script><link rel="stylesheet" type="text/css" href="<?= GlobalConfig::getAppPath() ?>/rsc/lib/bootstrap-3.3.7/css/bootstrap.css">
+<script src="<?= GlobalConfig::getAppPath() ?>/rsc/lib/bootstrap-3.3.7/js/bootstrap.js"></script>
 
 <link rel="stylesheet" type="text/css" href="<?= GlobalConfig::getAppPath() ?>/rsc/css/base.css">
 <script src="<?= GlobalConfig::getAppPath() ?>/rsc/js/base.js"></script>
