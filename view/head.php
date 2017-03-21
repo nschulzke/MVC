b7ed0da0f9e805ebe3d28c07d6a22e0a6ff140bf
Nathan Schulzke <nschulzke@gmail.com>
2017-03-20 21:36:06 -0600

    Split _includes into foot and head, so we can put scripts at the bottom.

---
+++ view/head.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" type="text/css" href="<?= GlobalConfig::getAppPath() ?>/rsc/lib/bootstrap-3.3.7/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="<?= GlobalConfig::getAppPath() ?>/rsc/css/base.css">
--- view/layout.php
+++ view/layout.php
@@ -1,23 +1,26 @@
 <?php
 $route = new Route();
+
+$controllerHead = __DIR__ . '/' . $route->getController() . '/_head.php';
+$actionHead = __DIR__ . '/' . $route->getController() . '/' . $route->getAction() . '_head.php';
+
+$controllerFoot = __DIR__ . '/' . $route->getController() . '/_foot.php';
+$actionFoot = __DIR__ . '/' . $route->getController() . '/' . $route->getAction() . '_foot.php';
 ?>
 <html>
     <head>
         <?php
-        require_once __DIR__ . '/_includes.php';
+        require_once __DIR__ . '/head.php';
                         
-        $controllerIncludes = __DIR__ . '/' . $route->getController() . '/_includes.php';
-        $actionIncludes = __DIR__ . '/' . $route->getController() . '/' . $route->getAction() . '_includes.php';
-
-        if (file_exists($controllerIncludes))
-            require_once $controllerIncludes;
+        if (file_exists($controllerHead))
+            require_once $controllerHead;
         
-        if (file_exists($actionIncludes))
-            require_once $actionIncludes;
+        if (file_exists($actionHead))
+            require_once $actionHead;
         ?>
     </head>
     <body>
-        <div class="container">
+        <div id="main-container" class="container">
             <header>
             </header>
             
@@ -25,6 +28,17 @@ $route = new Route();
             
             <footer>
             </footer>
+        </div>
+        <div id="scripts" class="hidden">
+        <?php
+        require_once __DIR__ . '/foot.php';
+
+        if (file_exists($controllerFoot))
+            require_once $controllerFoot;
+        
+        if (file_exists($actionFoot))
+            require_once $actionFoot;
+        ?>
         </div>
     </body>
 </html>
