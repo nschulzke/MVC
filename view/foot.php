0ceb9c2ce4ea92445086ce36ef9f5726dbe8db79
Nathan Schulzke <nschulzke@gmail.com>
2017-03-20 23:56:45 -0600

    Add tether to make bootstrap happy

--- view/foot.php
+++ view/foot.php
@@ -1,4 +1,5 @@
 <script src="<?= GlobalConfig::getAppPath() ?>/rsc/lib/jquery-3.2.0/jquery.js"></script>
+<script src="<?= GlobalConfig::getAppPath() ?>/rsc/lib/tether-1.3.3/js/tether.js"></script>
 <script src="<?= GlobalConfig::getAppPath() ?>/rsc/lib/bootstrap-4.0.0-alpha.6/js/bootstrap.js"></script>
 
 <script src="<?= GlobalConfig::getAppPath() ?>/rsc/js/base.js"></script>
--- view/head.php
+++ view/head.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+<link rel="stylesheet" type="text/css" href="<?= GlobalConfig::getAppPath() ?>/rsc/lib/tether-1.3.3/css/tether.css">
 <link rel="stylesheet" type="text/css" href="<?= GlobalConfig::getAppPath() ?>/rsc/lib/bootstrap-4.0.0-alpha.6/css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="<?= GlobalConfig::getAppPath() ?>/rsc/css/base.css">
