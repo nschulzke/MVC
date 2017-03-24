a98c00080f6d42422c137b061ee791caa80570b7
Nathan Schulzke <nschulzke@gmail.com>
2017-03-24 10:36:33 -0600

    PHPDoc

--- class/config/Database.php
+++ class/config/Database.php
@@ -15,21 +15,33 @@ class Database
     {
     }
 
+    /**
+     * @return string The host parameter for database connection
+     */
     public static function getHost()
     {
         return self::$HOST;
     }
 
+    /**
+     * @return string The dbname parameter for database connnection
+     */
     public static function getName()
     {
         return self::$NAME;
     }
 
+    /**
+     * @return string The username to use to connect
+     */
     public static function getUser()
     {
         return self::$USER;
     }
 
+    /**
+     * @return string The password to use to connect
+     */
     public static function getPass()
     {
         return self::$PASS;
