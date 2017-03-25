97893acb1328b1a8356b0783e4c296b504f915e5
Nathan Schulzke <nschulzke@gmail.com>
2017-03-24 20:43:20 -0600

    Restore highlights

--- view/_components/footer.php
+++ view/_components/footer.php
@@ -1,3 +1,3 @@
-<footer id="footer-container" class="container">
+<footer id="footer-container" class="container bg-faded">
     <span class="text-muted">&copy; 2017 Nathan Schulzke</span>
 </footer>
