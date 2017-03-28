d52d35b274cd2c5174888596f1ae9cbc402d2b7b
Nathan Schulzke <nschulzke@gmail.com>
2017-03-28 14:31:08 -0600

    Clean up HTML

--- view/scripture/book-list.php
+++ view/scripture/book-list.php
@@ -10,21 +10,19 @@ extract( $this->vars );
 ?>
 <div class="card">
     <div class="card-block">
-        <div id="books-accordion" class="accordion" role="tablist" aria-multiselectable="true">
-            <?php foreach ( $volumes as $volId => $data ): ?>
-                <div class="card">
-                    <div class="card-header" role="tab" id="volume_<?= $volId ?>" href="#books_<?= $volId ?>" data-toggle="collapse" data-parent="#books-accordion" aria-controls="books_<?= $volId ?>">
-                        <h5 class="mb-0">
-                            <?= $data['name'] ?>
-                        </h5>
-                    </div>
-                    <div class="collapse<?= $active == $volId ? ' show' : '' ?> card-block books-list" role="tabpanel" id="books_<?= $volId ?>" aria-labelledby="volume_<?= $volId ?>">
-                        <?php foreach ( $data['books'] as $id => $book ): ?>
-                            <a href="<?= $url ?>/<?= $id ?>" class="ajax-link"><?= $book ?></a>
-                        <?php endforeach ?>
-                    </div>
-                </div>
+        <div id="books-accordion" class="card accordion" role="tablist" aria-multiselectable="true">
+        <?php foreach ( $volumes as $volId => $data ): ?>
+            <div class="card-header" role="tab" id="volume_<?= $volId ?>" href="#books_<?= $volId ?>" data-toggle="collapse" data-parent="#books-accordion" aria-controls="books_<?= $volId ?>">
+                <h5 class="mb-0">
+                    <?= $data['name'] ?>
+                </h5>
+            </div>
+            <div class="collapse<?= $active == $volId ? ' show' : '' ?> card-block books-list" role="tabpanel" id="books_<?= $volId ?>" aria-labelledby="volume_<?= $volId ?>">
+            <?php foreach ( $data['books'] as $id => $book ): ?>
+                <a href="<?= $url ?>/<?= $id ?>" class="ajax-link"><?= $book ?></a>
             <?php endforeach ?>
+            </div>
+        <?php endforeach ?>
         </div>
     </div>
 </div>
