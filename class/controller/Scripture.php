58f90e67bc80a1e40bf4c732f387e81c080a93d4
Nathan Schulzke <nschulzke@gmail.com>
2017-03-30 18:26:25 -0600

    Add newFootnote action

--- class/controller/Scripture.php
+++ class/controller/Scripture.php
@@ -14,6 +14,8 @@ class Scripture
     const VIEW_VERSES = 'scripture/view.php';
     const VIEW_CHAPTERS = 'scripture/chapter-list.php';
     const VIEW_BOOKS = 'scripture/book-list.php';
+    const FOOTNOTE_PICK_WORD = 'scripture/footnote-pick-word.php';
+    const FOOTNOTE_PICK_TARGET = 'scripture/footnote-pick-target.php';
     const ROOT = Application::APP_PATH . '/scripture';
     
     static public function action_lookup( $route, $params )
@@ -108,7 +110,35 @@ class Scripture
         }
     }
     
-    static public function action_saveFootnote( $route, $params )
+    static public function action_newFootnote( $route, $params )
+    {
+        // No source verse selected
+        if ( empty( $params[0] ) )
+            echo "No verse selected";
+        else
+            $verse = MScripture::getVerseRepo()->find( $params[0] );
+        if ( empty( $verse ) )
+            echo "Invalid verse ID!";
+        else {
+            $view = new View( $route, 'none' );
+            // No word picked out yet
+            if ( empty( $params[1] ) )
+                $view->setVars( [
+                    'verse'    => $verse,
+                    'viewPath' => self::FOOTNOTE_PICK_WORD,
+                ] );
+            else
+                $view->setVars( [
+                    'verse'    => $verse,
+                    'word'     => $params[1],
+                    'viewPath' => self::FOOTNOTE_PICK_TARGET,
+                ] );
+            
+            $view->display();
+        }
+    }
+    
+    public static function action_saveFootnote( $route, $params )
     {
         $vars = [ 'verseId' => 'Verse', 'wordNumber' => 'Word', 'targetVerseId' => 'Target Verse' ];
         HTTP::requireVars( $vars );
